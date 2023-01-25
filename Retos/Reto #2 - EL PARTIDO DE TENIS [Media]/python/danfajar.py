4d2b56bfc08c3a34fd1600b457290c6726b83500
Daniel Fajardo <80439485+danfajar@users.noreply.github.com>
2023-01-25 03:03:10 +0000

    continuamos con el analisis del reto#2

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/danfajar.py
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/danfajar.py
@@ -4,7 +4,7 @@
  * gane cada punto del juego.
  * 
  * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
- * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+ * - Ante la secuencia ["P1", "P1", "P2", "P2", "P1", "P2", "P1", "P1"], el programa mostraría lo siguiente:
  *   15 - Love
  *   30 - Love
  *   30 - 15
@@ -16,5 +16,28 @@
  * - Si quieres, puedes controlar errores en la entrada de datos.   
  * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
 '''
+p1 = 0
+p2 = 0
+game1 = ["P1", "P1", "P2", "P2", "P1", "P2", "P1", "P1"]
 
-game1 = [P1, P1, P2, P2, P1, P2, P1, P1]
+points_rule = {0:'Love',1:'15', 2:'30', 3:'40', 4:'Deuce', 5:'Ventaja' }
+
+for i in game1:
+    if i == "P1":
+        p1 += 1
+        p1p = points_rule[p1]
+        p2p = points_rule[p2]
+        print(f'{p1p} - {p2p}')
+    else:
+        p2 += 1
+        p1p = points_rule[p1]
+        p2p = points_rule[p2]
+        print(f'{p1p} - {p2p}')
+
+def marcador(p1,p2):
+    
+    if p1 == 3 and p2 == 3:
+        print('Deuce')
+    elif p1 > 3 or p2 > 3:
+
+
